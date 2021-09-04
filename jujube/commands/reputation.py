5a6b8ddf756411213e906550e387e980b854b324
MAL22 <mimilavoie95@msn.com>
2021-09-04 03:11:47 -0400

    fixed rep cmd not using arg

--- jujube/commands/reputation.py
+++ jujube/commands/reputation.py
@@ -19,10 +19,13 @@ class ReputationCommand(Command, OnMessageInterface, OnReactionAddInterface, OnR
             if not args:
                 user_id = message.author.id
             else:
-                user_id = message.author.id
+                user_id = int(args[0].strip('<@!>'))
         except ValueError as error:
             log(error)
         else:
+            if not self.client.get_user(user_id):
+                return
+
             print('Command: {} {}'.format(command, user_id))
             user = DatabaseManager().verify_user_exists(user_id)
 
@@ -33,7 +36,7 @@ class ReputationCommand(Command, OnMessageInterface, OnReactionAddInterface, OnR
             if self.positive_emoji is None or self.negative_emoji is None:
                 raise ValueError(GlobalLanguageConfig().config['Errors']['emoji.missing'])
 
-            embed_msg = discord.Embed(title="test", description='<@{0}>'.format(user[0]))
+            embed_msg = discord.Embed(title=GlobalLanguageConfig().config['Commands']['ReputationCommandName'], description='<@{0}>'.format(user[0]))
             embed_msg.set_thumbnail(url=self.client.get_user(user[0]).avatar_url)
             embed_msg.add_field(name=('<:{}:{}>'.format(self.positive_emoji.name, self.positive_emoji.id)),
                                 value=user[1],
@@ -44,10 +47,36 @@ class ReputationCommand(Command, OnMessageInterface, OnReactionAddInterface, OnR
             await message.channel.send(embed=embed_msg)
 
     async def on_reaction_add(self, reaction, user, *args, **kwargs):
-        print(reaction)
+        if user == reaction.message.author:
+            return
+        author = DatabaseManager().verify_user_exists(reaction.message.author.id)
+
+        if reaction.emoji is self.positive_emoji:
+            if author is None:
+                DatabaseManager().insert_user(reaction.message.author.id, positive_rep=1)
+            else:
+                DatabaseManager().update_user(reaction.message.author.id, author[1] + 1, author[2])
+        elif reaction.emoji == self.negative_emoji:
+            if author is None:
+                DatabaseManager().insert_user(reaction.message.author.id, negative_rep=1)
+            else:
+                DatabaseManager().update_user(reaction.message.author.id, author[1], author[2] + 1)
 
     async def on_reaction_remove(self, reaction, user, *args, **kwargs):
-        pass
+        if user == reaction.message.author:
+            return
+        author = DatabaseManager().verify_user_exists(reaction.message.author.id)
+
+        if reaction.emoji is self.positive_emoji:
+            if author is None:
+                DatabaseManager().insert_user(reaction.message.author.id, positive_rep=1)
+            else:
+                DatabaseManager().update_user(reaction.message.author.id, author[1] - 1, author[2])
+        elif reaction.emoji == self.negative_emoji:
+            if author is None:
+                DatabaseManager().insert_user(reaction.message.author.id, negative_rep=1)
+            else:
+                DatabaseManager().update_user(reaction.message.author.id, author[1], author[2] - 1)
 
     def fetch_emoji(self, raw_emoji):
         if isinstance(raw_emoji, int):
