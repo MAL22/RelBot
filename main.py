3a41a91b8a92e9498feb7967b8f2b6fb03946b11
MAL22 <mimilavoie95@msn.com>
2021-09-21 15:14:12 -0400

    main timer instanciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import jujube.json.json_reader as json_reader
+from jujube.utils.timer import Timers
 from jujube.client import JujubeClient
 from jujube.app_config import GlobalAppConfig, GlobalLanguageConfig
 from jujube.database.database_manager import DatabaseManager
@@ -7,12 +8,13 @@ from jujube.database.database_manager import DatabaseManager
 intents = discord.Intents.default()
 intents.members = True
 intents.reactions = True
-print('main')
+
 app_cfg = GlobalAppConfig("config.json")
 lng_cfg = GlobalLanguageConfig(app_cfg.language)
 
+DatabaseManager()
+Timers()
+
 client = JujubeClient(intents=intents)
-guild = discord.Guild
-database = DatabaseManager()
 
 client.run(json_reader.read('token'))
